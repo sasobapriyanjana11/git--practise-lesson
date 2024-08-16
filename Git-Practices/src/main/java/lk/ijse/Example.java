4bad5d4ea449ff20936f5c7145a678c915ef18c7
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-08-16 22:06:40 +0530

    created exmple class

---
+++ Git-Practices/src/main/java/lk/ijse/Example.java
@@ -0,0 +1,12 @@
+package lk.ijse;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("create new branch");
+        NewBranch();
+    }
+
+    private static void NewBranch() {
+        System.out.println("git checkout -b registration");
+    }
+}
